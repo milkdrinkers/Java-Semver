886598454f3687609e62c25a9b91538a38a6806f
darksaid98 <darksaid9889@gmail.com>
2024-12-21 19:28:25 +0100

    ci: use publisher plugin

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,7 +3,7 @@ import java.time.Instant
 plugins {
 //    `maven-publish`
 //    signing
-    alias(libs.plugins.publish.on.central)
+    id("io.deepmedia.tools.deployer") version "0.16.0"
 }
 
 applyCustomVersion()
@@ -44,71 +44,138 @@ tasks {
     }
 }
 
-publishOnCentral {
-    projectLongName.set(rootProject.name)
-    projectDescription.set(rootProject.description.orEmpty())
-    projectUrl.set("https://github.com/milkdrinkers/java-semver")
+deployer {
+    release {
+        version.set("${rootProject.version}")
+        description.set(rootProject.description.orEmpty())
+    }
 
-    licenseName.set("GNU General Public License Version 3")
-    licenseUrl.set("https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
+    projectInfo {
+        groupId = "io.github.milkdrinkers"
+        artifactId = "javasemver"
+        version = "${rootProject.version}"
 
-    scmConnection.set("git:git://github.com/milkdrinkers/java-semver")
+        name = rootProject.name
+        description = rootProject.description.orEmpty()
+        url = "https://github.com/milkdrinkers/java-semver"
 
-    configureMavenCentral.set(true)
-    mavenCentral.user.set(System.getenv("MAVEN_USERNAME") ?: project.findProperty("maven.username")?.toString())
-    mavenCentral.password.set(System.getenv("MAVEN_PASSWORD") ?: project.findProperty("maven.password")?.toString())
+        scm {
+            connection = "scm:git:git://github.com/milkdrinkers/java-semver.git"
+            developerConnection = "scm:git:ssh://github.com:milkdrinkers/java-semver.git"
+            url = "https://github.com/milkdrinkers/java-semver"
+        }
 
-    if (rootProject.version.toString().endsWith("-SNAPSHOT")) { // Avoid stable versions being pushed there...
-        mavenCentralSnapshotsRepository() // Imports user and password from the configuration for Maven Central
-        // mavenCentralSnapshotsRepository() {
-        //     ...but they can be customized as per any other repository
-        // }
+        license({
+            name = "GNU General Public License Version 3"
+            url = "https://www.gnu.org/licenses/gpl-3.0.en.html#license-text"
+        })
+
+        developer({
+            name.set("darksaid98")
+            email.set("darksaid9889@gmail.com")
+            url.set("https://github.com/darksaid98")
+            organization.set("Milkdrinkers")
+        })
+    }
+
+    content {
+        component {
+            fromJava()
+        }
+    }
+
+    centralPortalSpec {
+        allowMavenCentralSync = false
+        auth.user.set(secret("MAVEN_USERNAME"))
+        auth.password.set(secret("MAVEN_PASSWORD"))
+    }
+
+    signing {
+        key.set(secret("GPG_KEY"))
+        password.set(secret("GPG_PASSWORD"))
     }
 }
 
-publishing {
-    publications {
-        withType<MavenPublication> {
-            groupId = "io.github.milkdrinkers"
-            artifactId = "javasemver"
-            version = "${rootProject.version}"
-
-            pom {
+//publishing {
+//    publications {
+//        create<MavenPublication>("maven") {
+//            groupId = "io.github.milkdrinkers"
+//            artifactId = "javasemver"
+//            version = "${rootProject.version}"
+//
+//            from(components["java"])
+//
+//            pom {
 //                name.set(rootProject.name)
 //                description.set(rootProject.description.orEmpty())
 //                url.set("https://github.com/milkdrinkers/java-semver")
+//                inceptionYear.set("2024")
+//
 //                licenses {
 //                    license {
 //                        name.set("GNU General Public License Version 3")
 //                        url.set("https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
 //                    }
 //                }
-                developers {
-                    developer {
-                        id.set("darksaid98")
-                        name.set("darksaid98")
-                        email.set("darksaid9889@gmail.com")
-                        url.set("https://github.com/darksaid98")
-                        organization.set("Milkdrinkers")
-                        organizationUrl.set("https://github.com/milkdrinkers")
-                    }
-                }
+//
+//                developers {
+//                    developer {
+//                        id.set("darksaid98")
+//                        name.set("darksaid98")
+//                        email.set("darksaid9889@gmail.com")
+//                        url.set("https://github.com/darksaid98")
+//                        organization.set("Milkdrinkers")
+//                        organizationUrl.set("https://github.com/milkdrinkers")
+//                    }
+//                }
+//
 //                scm {
 //                    connection.set("scm:git:git://github.com/milkdrinkers/java-semver.git")
 //                    developerConnection.set("scm:git:ssh://github.com:milkdrinkers/java-semver.git")
 //                    url.set("https://github.com/milkdrinkers/java-semver")
 //                }
-            }
-        }
-    }
-}
+//            }
+//        }
+//    }
+//
+//    repositories {
+//        maven {
+//            url = uri(layout.buildDirectory.dir("staging-deploy"))
+//        }
+//    }
+//}
 
-signing {
-    val signingKey = project.findProperty("signing.key")?.toString() ?: System.getenv("GPG_KEY")
-    val signingPassword = project.findProperty("signing.password")?.toString() ?: System.getenv("GPG_PASSWORD")
 
-    useInMemoryPgpKeys(signingKey, signingPassword)
-}
+
+//jreleaser {
+//    signing {
+//        active.set(Active.ALWAYS)
+//        armored.set(true)
+//        secretKey.set(rootProject.findProperty("signing.key")?.toString() ?: System.getenv("GPG_KEY"))
+//        passphrase.set(rootProject.findProperty("signing.password")?.toString() ?: System.getenv("GPG_PASSWORD"))
+//    }
+//    deploy {
+//        maven {
+//            mavenCentral {
+//                create("sonatype") {
+//                    active.set(Active.ALWAYS)
+//                    username.set(rootProject.findProperty("maven.username")?.toString() ?: System.getenv("MAVEN_USERNAME"))
+//                    password.set(rootProject.findProperty("maven.password")?.toString() ?: System.getenv("MAVEN_PASSWORD"))
+//                    url.set("https://central.sonatype.com/api/v1/publisher")
+//                    stagingRepository("target/staging-deploy")
+//                    applyMavenCentralRules.set(true)
+//                }
+//            }
+//        }
+//    }
+//}
+
+//signing {
+//    val signingKey = project.findProperty("signing.key")?.toString() ?: System.getenv("GPG_KEY")
+//    val signingPassword = project.findProperty("signing.password")?.toString() ?: System.getenv("GPG_PASSWORD")
+//
+//    useInMemoryPgpKeys(signingKey, signingPassword)
+//}
 
 fun applyCustomVersion() {
     // Apply custom version arg or append snapshot version
