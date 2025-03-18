7a845134aefc70d64104f207d978f8e1f176f6bc
darksaid98 <darksaid9889@gmail.com>
2025-03-18 07:16:56 +0100

    build: standardize build file

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,48 +1,9 @@
 import com.vanniktech.maven.publish.JavaLibrary
 import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.SonatypeHost
-import java.time.Instant
 
 plugins {
     alias(libs.plugins.publisher)
-}
-
-applyCustomVersion()
-
-dependencies {
-    compileOnly(libs.annotations)
-    annotationProcessor(libs.annotations)
-
-    testImplementation(libs.annotations)
-    testImplementation(platform(libs.junit.bom))
-    testImplementation(libs.bundles.junit)
-}
-
-tasks {
-    compileJava {
-        options.encoding = Charsets.UTF_8.name()
-        options.compilerArgs.addAll(arrayListOf("-Xlint:all", "-Xlint:-processing", "-Xdiags:verbose"))
-    }
-
-    javadoc {
-        isFailOnError = false
-        val options = options as StandardJavadocDocletOptions
-        options.encoding = Charsets.UTF_8.name()
-        options.overview = "src/main/javadoc/overview.html"
-        options.windowTitle = "${rootProject.name} Javadoc"
-        options.tags("apiNote:a:API Note:", "implNote:a:Implementation Note:", "implSpec:a:Implementation Requirements:")
-        options.addStringOption("Xdoclint:none", "-quiet")
-        options.use()
-    }
-
-    processResources {
-        filteringCharset = Charsets.UTF_8.name()
-    }
-
-    test {
-        useJUnitPlatform()
-        failFast = false
-    }
 }
 
 mavenPublishing {
@@ -91,12 +52,4 @@ mavenPublishing {
 
     // Skip signing for local tasks
     tasks.withType<Sign>().configureEach { onlyIf { !gradle.taskGraph.allTasks.any { it is PublishToMavenLocal } } }
-}
-
-fun applyCustomVersion() {
-    // Apply custom version arg or append snapshot version
-    val ver = properties["altVer"]?.toString() ?: "${rootProject.version}-SNAPSHOT.${Instant.now().epochSecond}"
-
-    // Strip prefixed "v" from version tag
-    rootProject.version = (if (ver.first().equals('v', true)) ver.substring(1) else ver.uppercase()).uppercase()
 }
