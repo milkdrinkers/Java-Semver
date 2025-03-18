cea1d683b7a6b1f8ba8361b86d1bc9b294922a4f
darksaid98 <darksaid9889@gmail.com>
2025-03-18 06:40:32 +0100

    build: snapshot jars should follow semver

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -95,7 +95,7 @@ mavenPublishing {
 
 fun applyCustomVersion() {
     // Apply custom version arg or append snapshot version
-    val ver = properties["altVer"]?.toString() ?: "${rootProject.version}-SNAPSHOT-${Instant.now().epochSecond}"
+    val ver = properties["altVer"]?.toString() ?: "${rootProject.version}-SNAPSHOT.${Instant.now().epochSecond}"
 
     // Strip prefixed "v" from version tag
     rootProject.version = (if (ver.first().equals('v', true)) ver.substring(1) else ver.uppercase()).uppercase()
