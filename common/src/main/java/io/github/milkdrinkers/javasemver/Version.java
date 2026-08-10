446bf2dca74d9d851dbeed191ed33bfaeaed1320
darksaid98 <darksaid9889@gmail.com>
2026-08-10 16:45:22 +0200

    refactor: move builder inside Version class

--- common/src/main/java/io/github/milkdrinkers/javasemver/Version.java
+++ common/src/main/java/io/github/milkdrinkers/javasemver/Version.java
@@ -161,7 +161,8 @@ public class Version implements Comparable<Version> {
      * @param patch the patch version
      * @return the semantic version
      * @throws VersionBuildException thrown if parsing the data into a version failed
-     * @apiNote Uses {@link VersionBuilder} internally
+     * @apiNote Uses {@link Builder} internally
+     * @see #builder()
      */
     public static @NotNull Version of(long major, long minor, long patch) throws VersionBuildException {
         return of(major, minor, patch, "");
@@ -176,7 +177,8 @@ public class Version implements Comparable<Version> {
      * @param preRelease the pre-release version
      * @return the semantic version
      * @throws VersionBuildException thrown if parsing the data into a version failed
-     * @apiNote Uses {@link VersionBuilder} internally
+     * @apiNote Uses {@link Builder} internally
+     * @see #builder()
      */
     public static @NotNull Version of(long major, long minor, long patch, String preRelease) throws VersionBuildException {
         return of(major, minor, patch, preRelease, "");
@@ -192,10 +194,11 @@ public class Version implements Comparable<Version> {
      * @param meta       the build-meta
      * @return the semantic version
      * @throws VersionBuildException thrown if parsing the data into a version failed
-     * @apiNote Uses {@link VersionBuilder} internally
+     * @apiNote Uses {@link Builder} internally
+     * @see #builder()
      */
     public static @NotNull Version of(long major, long minor, long patch, String preRelease, String meta) throws VersionBuildException {
-        return new VersionBuilder()
+        return builder()
             .withMajor(major)
             .withMinor(minor)
             .withPatch(patch)
@@ -370,7 +373,7 @@ public class Version implements Comparable<Version> {
     public @NotNull Version increment(@NotNull ReleaseType release, @Nullable String identifier) throws VersionBuildException {
         Objects.requireNonNull(release, "release type must not be null");
 
-        final VersionBuilder builder = new VersionBuilder();
+        final Builder builder = Version.builder();
 
         switch (release) {
             case MAJOR:
@@ -691,5 +694,117 @@ public class Version implements Comparable<Version> {
     @Override
     public String toString() {
         return getVersionFull();
+    }
+
+    /**
+     * Creates a new {@link Builder} for constructing a {@link Version} object.
+     *
+     * @return the version builder
+     */
+    public static @NotNull Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * A class allowing the creation of {@link Version} objects through this builder.
+     *
+     * @implSpec Major, Minor and Patch are required in a valid semantic version.
+     */
+    public static final class Builder {
+        private Long major; // The Major version
+        private Long minor; // The Minor version
+        private Long patch; // The Patch version
+        private String preRelease; // The pre-release data like "SNAPSHOT-1" or "RC-3"
+        private String meta; // The build-metadata
+
+        /**
+         * Set major version for builder.
+         *
+         * @param major the major
+         * @return the version builder
+         */
+        public Builder withMajor(long major) {
+            this.major = major;
+            return this;
+        }
+
+        /**
+         * Set minor version for builder.
+         *
+         * @param minor the minor
+         * @return the version builder
+         */
+        public Builder withMinor(long minor) {
+            this.minor = minor;
+            return this;
+        }
+
+        /**
+         * Set patch version for builder.
+         *
+         * @param patch the patch
+         * @return the version builder
+         */
+        public Builder withPatch(long patch) {
+            this.patch = patch;
+            return this;
+        }
+
+        /**
+         * Set pre-release version for builder.
+         *
+         * @param preRelease the pre-release
+         * @return the version builder
+         */
+        public Builder withPreRelease(String preRelease) {
+            this.preRelease = preRelease;
+            return this;
+        }
+
+        /**
+         * Set build-metadata version for builder.
+         *
+         * @param meta the build-metadata
+         * @return the version builder
+         */
+        public Builder withMeta(String meta) {
+            this.meta = meta;
+            return this;
+        }
+
+        /**
+         * Build version.
+         *
+         * @return the {@link Version}
+         * @throws VersionBuildException thrown if the version could not be built
+         * @implSpec Major, Minor and Patch are required when building a valid semantic version.
+         */
+        public Version build() throws VersionBuildException {
+            if (major == null)
+                throw new VersionBuildException("Major version needs to be specified.");
+
+            if (minor == null)
+                throw new VersionBuildException("Minor version needs to be specified.");
+
+            if (patch == null)
+                throw new VersionBuildException("Patch version needs to be specified.");
+
+            if (major < 0L)
+                throw new VersionBuildException(String.format("Major version \"%s\" can't be less than 0.", major));
+
+            if (minor < 0L)
+                throw new VersionBuildException(String.format("Minor version \"%s\" can't be less than 0.", minor));
+
+            if (patch < 0L)
+                throw new VersionBuildException(String.format("Patch version \"%s\" can't be less than 0.", patch));
+
+            if (preRelease == null)
+                preRelease = "";
+
+            if (meta == null)
+                meta = "";
+
+            return new Version(major, minor, patch, preRelease, meta);
+        }
     }
 }
